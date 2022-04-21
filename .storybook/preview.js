3b8596ed9f83032ea12a6dd8f71814639edeee38
soopiri <soopiri.jay@gmail.com>
2022-04-21 16:35:36 +0900

    🧑‍💻 Add Custom DarkMode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,76 @@
-import React from "react";
+import { useEffect, useMemo } from "react";
 import { GlobalStyle } from "../src/shared/global";
+import { themes as storyBookThemes } from '@storybook/theming'
+import { ThemeProvider } from 'styled-components'
+import { useDarkMode } from 'storybook-dark-mode'
+import themes from "../src/themes";
+
+const ThemeWrapper = ({ children }) => {
+  const theme = themes[useDarkMode() ? 'dark' : 'light']
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+const addBackgroundStyle = (selector, css, storyId) => {
+  const existingStyle = document.getElementById(selector)
+  if (existingStyle) {
+    if (existingStyle.innerHTML !== css) {
+      existingStyle.innerHTML = css
+    }
+  } else {
+    const style = document.createElement('style')
+    style.setAttribute('id', selector)
+    style.innerHTML = css
+
+    const gridStyleSelector = `addon-backgrounds-grid${storyId ? `-docs-${storyId}` : ''
+      }`
+    // If grids already exist, we want to add the style tag BEFORE it so the background doesn't override grid
+    const existingGridStyle = document.getElementById(gridStyleSelector)
+    if (existingGridStyle) {
+      existingGridStyle.parentElement.insertBefore(style, existingGridStyle)
+    } else {
+      document.head.appendChild(style)
+    }
+  }
+}
+const BackgroundWrapper = ({ children, context }) => {
+  const selector =
+    context.viewMode === 'docs'
+      ? `#anchor--${context.id} .docs-story`
+      : '.sb-show-main'
+
+  const theme = themes[useDarkMode() ? 'dark' : 'light']
+  const selectedBackgroundColor = theme.background
+  const backgroundStyles = useMemo(() => {
+    return `
+      ${selector} {
+        background: ${selectedBackgroundColor} !important;
+        transition: background-color 0.3s;
+      }
+    `
+  }, [selectedBackgroundColor, selector])
+
+  useEffect(() => {
+    const selectorId =
+      context.viewMode === 'docs'
+        ? `addon-backgrounds-docs-${context.id}`
+        : `addon-backgrounds-color`
+
+    addBackgroundStyle(
+      selectorId,
+      backgroundStyles,
+      context.viewMode === 'docs' ? context.id : null
+    )
+  }, [backgroundStyles, context])
+
+  return <>{children}</>
+}
 
 export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyle />
-      <Story />
-    </>
-  ),
+  (renderStory, context) => (
+    <BackgroundWrapper context={context}>
+      <ThemeWrapper>{renderStory()}</ThemeWrapper>
+    </BackgroundWrapper>
+  )
 ];
 
 export const parameters = {
@@ -18,4 +81,9 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  darkMode: {
+    current: 'light',
+    dark: { ...storyBookThemes.dark, appBg: themes.dark.background },
+    light: { ...storyBookThemes.light, appBg: themes.light.background }
+  }
 };
--- src/shared/global.js
+++ src/shared/global.js
@@ -1,5 +1,4 @@
 import { createGlobalStyle, css } from "styled-components";
-import { color, typography } from "./styles";
 
 export const fontUrl =
   "https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900";
